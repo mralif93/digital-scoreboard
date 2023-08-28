c97113408cf42e44b15642bd553d14c3fd2674ea
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2023-08-28 23:32:04 +0800

    commit message

--- board/urls.py
+++ board/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.IndexView, name='index_view'),
+  path('', views.Page1View, name='page_1_view'),
+  path('v2/', views.Page2View, name='page_2_view'),
 ]
--- board/views.py
+++ board/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render
 from django.utils import timezone
 
 # Create your views here.
-def IndexView(request):
+def Page1View(request):
   now = timezone.now()
   context = {
     # 'now': now,
   }
-  return render(request, 'board/index.html', context)
+  return render(request, 'board/page1.html', context)
+
+
+def Page2View(request):
+  # creating class
+  class obj:
+    def __init__(self, title, total):
+      self.title = title
+      self.total = total
+
+  # creating list
+  list = []
+
+  # appending instances to list
+  list.append(obj('Lost Time Injury', 0))
+  list.append(obj('Medical Treatment Case', 0))
+  list.append(obj('First Aid Case', 0))
+  list.append(obj('Fire / Explosion', 0))
+  list.append(obj('Lost of Primary Containment', 0))
+  list.append(obj('Property / Equipment', 0))
+  list.append(obj('Near Miss', 0))
+  list.append(obj('Unsafe Act Unsafe Condition', 0))
+
+  context = {
+    'list': list,
+  }
+  return render(request, 'board/page2.html', context)
