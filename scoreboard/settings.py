a52489dfd3f0ee9f696970be4ed9376e03f8e732
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2023-08-29 09:58:52 +0800

    commit message

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -90,7 +90,12 @@ WSGI_APPLICATION = 'scoreboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
